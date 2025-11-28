7b73f2514d908f0f4b3278aca93e5c9f34f8b5db
Alexandra Ispas <aispas33@gmail.com>
2025-11-28 16:55:19 +0200

    variables

--- learning_go_book/hello.go
+++ learning_go_book/hello.go
@@ -5,4 +5,28 @@ import "fmt"
 func main() {
 
 	fmt.Printf("Hello")
+
+	var myLastInitial rune = 'B'
+	fmt.Println(myLastInitial)
+
+	// declaration using type inference. this can only be used inside functions
+	a := 10
+	println(a)
+
+	// declaration lists. to be used outside functions
+	var (
+		x    int
+		y        = 20
+		z    int = 30
+		d, e     = 40, "hello"
+		f, g string
+	)
+
+	println(x, y, z, d, e, f, g)
+
+	// constants
+	const (
+		idKey   = "id"
+		nameKey = "name"
+	)
 }
